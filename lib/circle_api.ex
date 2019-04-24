e068ba8ebdbc991e7faedfa03cddd400399258d7
David McGaffin <mcgaffin@gmail.com>
2019-04-24 17:54:13 -0400

    very simple build status summary!

--- lib/circle_api.ex
+++ lib/circle_api.ex
@@ -11,8 +11,7 @@ defmodule CircleApi do
   end
 
   def fetch_status do
-    # circle_url = "https://circle2.bubtools.net/api/v1.1/project/github/BookBub/lello/tree/dm-education-quiz"
-    circle_url = "https://circle2.bubtools.net/api/v1.1/me"
+    circle_url = "https://circle2.bubtools.net/api/v1.1/project/github/BookBub/lello/tree/dm-education-quiz"
     circle_token = "a598c3e26d9b3bbf0fe1d44a1a045236e2522f77"
     response = HTTPotion.get(circle_url,
       query: %{"circle-token" => circle_token},
@@ -20,6 +19,13 @@ defmodule CircleApi do
 
     {_, build_info} = Poison.decode(response.body)
     build_info
+  end
+
+  def fetch_build_results do
+    build_status = fetch_status()
+    results = Enum.map(build_status,
+      fn build -> "#{build["build_parameters"]["CIRCLE_JOB"]} => #{build["outcome"]}" end)
+    Enum.each(Enum.uniq(Enum.sort(results)), fn r -> IO.puts(r) end)
   end
 end
 
