8f0e24f828410ea92be16e9099364102d4b457b3
David McGaffin <mcgaffin@gmail.com>
2019-04-26 09:54:26 -0400

    update flow a bit

--- lib/circle_api.ex
+++ lib/circle_api.ex
@@ -10,8 +10,8 @@ defmodule CircleApi do
     me_info
   end
 
-  def fetch_status do
-    circle_url = "https://circle2.bubtools.net/api/v1.1/project/github/BookBub/lello/tree/dm-education-quiz"
+  defp fetch_status(branch_name) do
+    circle_url = "https://circle2.bubtools.net/api/v1.1/project/github/BookBub/lello/tree/#{branch_name}"
     circle_token = "a598c3e26d9b3bbf0fe1d44a1a045236e2522f77"
     response = HTTPotion.get(circle_url,
       query: %{"circle-token" => circle_token},
@@ -21,19 +21,35 @@ defmodule CircleApi do
     build_info
   end
 
-  def fetch_build_summary do
-    build_status = fetch_status()
-    results = Enum.map(build_status,
-      fn build -> "#{build["build_parameters"]["CIRCLE_JOB"]} => #{build["status"]}" end)
+  defp removeSuccessStates(builds) do
+    Enum.reject(builds, fn build -> build["status"] == "success" end)
+  end
+
+  defp gather_build_state_messages(builds) do
+    Enum.map(builds,
+      fn build ->
+        "#{build["build_parameters"]["CIRCLE_JOB"]} => #{build["status"]}"
+      end)
+  end
+
+  def print_build_summary(branch_name) do
+    build_status = fetch_status(branch_name)
+
+    results = build_status |> removeSuccessStates |> gather_build_state_messages
 
     first_build = Enum.at(build_status, 0)
     IO.puts ""
-    IO.puts("============================================================")
+    IO.puts("------------------------------------------------------------")
+    IO.puts("        branch: #{branch_name}")
     IO.puts("     committed: #{first_build["committer_date"]}")
     IO.puts("commit message: #{first_build["subject"]}")
-    IO.puts("============================================================")
+    IO.puts("------------------------------------------------------------")
 
-    results |> Enum.uniq |> Enum.reverse |> Enum.each(fn r -> IO.puts(r) end)
+    if Enum.empty?(results) do
+      IO.puts("build succeeded")
+    else
+      results |> Enum.uniq |> Enum.reverse |> Enum.each(fn r -> IO.puts(r) end)
+    end
 
     IO.puts ""
   end
--- lib/circli.ex
+++ lib/circli.ex
@@ -30,6 +30,6 @@ defmodule Circli do
   def main(args) do
     # args |> parse_args |> process
 
-    CircleApi.fetch_build_summary
+    CircleApi.print_build_summary(Enum.at(args, 0))
   end
 end
