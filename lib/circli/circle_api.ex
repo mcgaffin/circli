066f585239fca8b070ba3ad21aea5c4e448dd8db
David McGaffin <mcgaffin@gmail.com>
2019-04-26 15:25:32 -0400

    Restructure project to standards (#3)
    * conform to elixir standards; add a test
    
    * restore previously ignored files...?
    
    * improved usage and display of circle api output
    
    * improve function structure

---
+++ lib/circli/circle_api.ex
@@ -0,0 +1,87 @@
+defmodule Circli.CircleApi do
+  use Timex
+
+  def fetch_me do
+    circle_url = "https://circle2.bubtools.net/api/v1.1/me"
+    circle_token = "a598c3e26d9b3bbf0fe1d44a1a045236e2522f77"
+    response = HTTPotion.get(circle_url,
+      query: %{"circle-token" => circle_token},
+      headers: %{accept: "application/json"})
+
+    {_, me_info} = Poison.decode(response.body)
+    me_info
+  end
+
+  defp fetch_status(branch_name) do
+    circle_url = "https://circle2.bubtools.net/api/v1.1/project/github/BookBub/lello/tree/#{branch_name}"
+    circle_token = "a598c3e26d9b3bbf0fe1d44a1a045236e2522f77"
+
+    response = HTTPotion.get(circle_url,
+      query: %{
+        "circle-token" => circle_token,
+        limit: 15,
+        shallow: true,
+      },
+      headers: %{accept: "application/json"})
+
+    {_, build_info} = Poison.decode(response.body)
+    build_info
+  end
+
+  defp removeSuccessStates(builds) do
+    Enum.reject(builds, fn build -> build["status"] == "success" end)
+  end
+
+  defp gather_build_state_messages(builds) do
+    Enum.map(builds,
+      fn build ->
+        "#{build["workflows"]["job_name"]} => #{build["status"]}"
+      end)
+  end
+
+  def generate_build_results(branch_name) do 
+    build_states = fetch_status(branch_name)
+
+    messages = build_states
+              |> removeSuccessStates
+              |> gather_build_state_messages
+
+    commit_date = build_states
+                  |> Enum.find(fn build -> build["committer_date"] != nil end)
+                  |> Map.get("committer_date")
+                  |> Timex.parse!("{ISO:Extended}")
+
+    commit_message = build_states
+                     |> Enum.find(fn build -> build["subject"] != nil end)
+                     |> Map.get("subject")
+
+    %{
+      messages: messages,
+      commit_date: commit_date,
+      commit_message: commit_message,
+    }
+  end
+
+  def print_build_summary(branch_name) do
+    results = generate_build_results(branch_name)
+
+    IO.puts ""
+    IO.puts("------------------------------------------------------------")
+    IO.puts("        branch: #{branch_name}")
+    IO.puts("     committed: #{Timex.format!(results[:commit_date], "{relative}", :relative)}")
+    IO.puts("commit message: #{results[:commit_message]}")
+    IO.puts("------------------------------------------------------------")
+
+    if Enum.empty?(results[:messages]) do
+      IO.puts("build succeeded")
+    else
+      results[:messages]
+      |> Enum.uniq
+      |> Enum.reverse
+      |> Enum.each(fn r -> IO.puts(r) end)
+    end
+
+    IO.puts ""
+  end
+end
+
---
+++ lib/circli/cli.ex
@@ -0,0 +1,41 @@
+defmodule Circli.CLI do
+  @moduledoc """
+  Handle the command line parsing and the dispatch to the various functions that end up
+  generating a table of the last _n_ issues in a github project
+  """
+  def main(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  defp process(:help) do
+    IO.puts """
+
+      Usage: circli <lello-branch-name>
+
+    """
+    System.halt(0)
+  end
+
+  defp process({ branch_name }) do
+    Circli.CircleApi.print_build_summary(branch_name)
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+    Otherwise it is a lello branch-name
+
+  Prints a build summary for the latest commit of the provided branch.
+  """
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [ help: :boolean], aliases: [ h: :help ])
+    case parse do
+      { [ help: true ], _, _ }
+      -> :help
+        { _, [ branch_name ], _ }
+        -> { branch_name }
+      _ -> :help
+    end
+  end
+end
