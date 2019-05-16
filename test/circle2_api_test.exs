6bcef46f8de6c712fcb6d52e915694ec80952794
David McGaffin <mcgaffin@gmail.com>
2019-05-16 10:06:00 -0400

    add capture_io tests

--- test/circle2_api_test.exs
+++ test/circle2_api_test.exs
@@ -1,17 +1,32 @@
 defmodule Circle2ApiTest do
   use ExUnit.Case
   doctest Circli.Circle2Api
+
+  import ExUnit.CaptureIO
   import Circli.Circle2Api
 
-  test "everything is ok" do
-    assert print_build_messages([]) === :ok
+  test "prints success message when no builds have failed or are still running" do
+    fun = fn ->
+      assert print_build_messages([]) === :ok
+    end
+
+    assert capture_io(fun) == "√ build succeeded\n"
   end
 
   test "returns empty array and prints message if no builds were found" do
-    assert most_recent_workflow([]) === []
+    fun = fn ->
+      assert most_recent_workflow([]) === []
+    end
+
+    assert capture_io(fun) == "\nThere are no builds on this branch yet. Get busy!\n\n"
   end
 
   test "returns empty array and prints error message if builds could not be fetched" do
-    assert most_recent_workflow([{ :error, "{ :conn_refused, :error }" }]) === []
+    error_message = "{ :conn_refused, :error }"
+    fun = fn ->
+      assert most_recent_workflow([{ :error, error_message }]) === []
+    end
+
+    assert capture_io(fun) == "\nThere was an error fetching the build status: #{error_message}\n\n"
   end
 end
